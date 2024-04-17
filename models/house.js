32207f30b3217c2e15452aa1093dfc2505504faa
spraneeth28 <156373777+spraneeth28@users.noreply.github.com>
2024-04-17 17:05:59 -0500

    updated

--- models/house.js
+++ models/house.js
@@ -1,8 +1,26 @@
 const mongoose = require("mongoose")
+
 const houseSchema = mongoose.Schema({
-house_number: Number,
-type_of_house: String,
-location: String
-})
+    house_number: {
+      type: Number,
+      required: true,
+      min: 10,
+      max: 2999,
+    },
+    type_of_house: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 100,
+    },
+    location: {
+      type: String,
+      required: true,
+      minlength: 7,
+      maxlength: 100,
+    },
+
+  });
+
 module.exports = mongoose.model("house",
 houseSchema)
--- routes/house.js
+++ routes/house.js
@@ -2,16 +2,26 @@ var express = require('express');
 const house_controlers= require('../controllers/house');
 var router = express.Router();
 
+// A little function to check if we have an authorized user and continue on
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+res.redirect("/login");
+}
+
 /* GET home page. */
 router.get('/', house_controlers.house_view_all_Page );
 
 
 //Assignment 12 ( Part -4)
 //* GET detail costume page */
-router.get('/detail', house_controlers.house_view_one_Page);
-router.get('/create', house_controlers.house_create_Page);
-router.get('/update', house_controlers.house_update_Page);
-router.get('/delete', house_controlers.house_delete_Page);
+router.get('/detail', secured, house_controlers.house_view_one_Page);
+router.get('/create', secured, house_controlers.house_create_Page);
+router.get('/update', secured, house_controlers.house_update_Page);
+router.get('/delete', secured, house_controlers.house_delete_Page);
 
 
 module.exports = router;
